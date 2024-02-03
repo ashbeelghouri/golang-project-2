90483d78d5d7cd95c38ccd089f385e336991f813
Ashbeel Ghouri <ashbeelghouri@protonmail.com>
2024-02-03 07:16:15 +0400

    created channels

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+func main() {
+	M := 8
+	N := 2
+
+	// Channels for communication between readers and writers
+	readerChannel := make(chan []byte, 1)
+	writerChannel := make(chan []byte, 1)
+
+}
