eb458e83d888ea480cffdc1ec6c57e2e152862ca
Ashbeel Ghouri <ashbeelghouri@protonmail.com>
2024-02-03 07:29:16 +0400

    updated for error

--- main.go
+++ main.go
@@ -1,11 +1,32 @@
 package main
 
-func main() {
-	M := 8
-	N := 2
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
-	// Channels for communication between readers and writers
-	readerChannel := make(chan []byte, 1)
-	writerChannel := make(chan []byte, 1)
+func main() {
+	const M = 8
+	const N = 2
+	const BufferSize = 10
+
+	var buffer = make([]int, BufferSize)
+	var mutex = &sync.Mutex{}
+	var readCh = make(chan bool)
+
+	// Writing goroutines
+	for i := 0; i < N; i++ {
+		go func(writerID int) {
+			for {
+				mutex.Lock()
+				buffer[0]++ // Simulate writing to the buffer
+				fmt.Printf("Writer %d wrote to buffer\n", writerID)
+				mutex.Unlock()
+				readCh <- true // Signal that data is written
+				time.Sleep(1 * time.Second)
+			}
+		}(i)
+	}
 
 }
